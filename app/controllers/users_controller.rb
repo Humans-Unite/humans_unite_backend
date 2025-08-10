6bd8ec990eba0d93207f65834a0bac91899e3fb2
Md Mahfuz Alam <mahfuzalamdestiny@gmail.com>
2025-08-10 21:33:41 +0530

    remove authentication from users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/users_controller.rb
 class UsersController < ApplicationController
-  before_action :authenticate_user! 
+  # before_action :authenticate_user! 
 
   def index
     @users = User.all
